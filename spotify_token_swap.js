368f8bd74f29b2fee8e4d1ac1846ffa787cfedd3
Jesper Svennevid <github@svennevid.se>
2014-11-26 08:21:58 +0100

    Added config file support Added some debug logging Added debug endpoint for token dispatch

--- spotify_token_swap.js
+++ spotify_token_swap.js
@@ -3,31 +3,31 @@ var express = require("express"),
     request = require("request"),
     crypto = require('crypto'),
     url = require('url'),
+    config = require('config'),
+    debug = require('debug')('spotiy-token-swap'),
     bodyParser = require("body-parser");
 
-var CLIENT_ID = "e6695c6d22214e0f832006889566df9c";
-var CLIENT_SECRET = "29eb02041ba646179a1189dccac112c7";
-var ENCRYPTION_SECRET = "cFJLyifeUJUBFWdHzVbykfDmPHtLKLGzViHW9aHGmyTLD8hGXC";
-var CLIENT_CALLBACK_URL = "spotifyiossdkexample://";
-var AUTH_HEADER = "Basic " + new Buffer(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64");
-var SPOTIFY_ACCOUNTS_ENDPOINT = "https://accounts.spotify.com";
+var AUTH_HEADER = "Basic " + new Buffer(config.client_id + ":" + config.client_secret).toString("base64");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/swap", function (req, res) {
     if (!req.body || !req.body.hasOwnProperty("code")) {
+        debug("swap: missing auth code");
         res.status(550).send("Permission Denied");
         return;
     }
 
     var form_data = {
         "grant_type": "authorization_code",
-        "redirect_uri": CLIENT_CALLBACK_URL,
+        "redirect_uri": config.callback_url,
         "code": req.body.code
     };
 
+    debug("swap: POSTing request to %s:", url.resolve(config.endpoint, "/api/token"), form_data);
+
     request.post({
-        "url": url.resolve(SPOTIFY_ACCOUNTS_ENDPOINT, "/api/token"),
+        "url": url.resolve(config.endpoint, "/api/token"),
         headers: {
             "Authorization": AUTH_HEADER,
             "Content-Type": "application/x-www-form-urlencoded"
@@ -35,19 +35,24 @@ app.post("/swap", function (req, res) {
         form: form_data
     }, function (err, response, body) {
         if (err) {
+            debug("swap: error: %s", err);
             res.status(500).send("Internal Server Error");
             return;
         }
 
         if (response.statusCode != 200) {
+            debug("swap: response: %s", response.statusCode);
             res.status(550).send("Permission Denied");
             return;
         }
 
         var token_data = JSON.parse(body);
 
-        var cipher = crypto.createCipher("DES-EDE3-CBC", ENCRYPTION_SECRET);
+        var cipher = crypto.createCipher("DES-EDE3-CBC", config.encryption_secret);
         var encrypted_token = Buffer.concat([cipher.update(token_data.refresh_token), cipher.final()]).toString("base64");
+
+        debug("swap: refresh_token: %s", token_data.refresh_token);
+        debug("swap: encrypted_token: %s", encrypted_token);
 
         res.send({
             "refresh_token": encrypted_token
@@ -65,16 +70,21 @@ app.post("/refresh", function (req, res) {
 
     var encrypted_token = new Buffer(req.body.refresh_token, 'base64');
 
-    var decipher = crypto.createDecipher("DES-EDE3-CBC", ENCRYPTION_SECRET);
+    var decipher = crypto.createDecipher("DES-EDE3-CBC", config.encryption_secret);
     var refresh_token = Buffer.concat([decipher.update(encrypted_token), decipher.final()]).toString();
+
+    debug("refresh: encrypted_token: %s", req.body.refresh_token);
+    debug("refresh: refresh_token: %s", refresh_token);
 
     var form_data = {
         "grant_type": "refresh_token",
         "refresh_token": refresh_token
     };
 
+    debug("refresh: POSTing request to %s:", url.resolve(config.endpoint, "/api/token"), form_data);
+
     request.post({
-        url: url.resolve(SPOTIFY_ACCOUNTS_ENDPOINT, "/api/token"),
+        url: url.resolve(config.endpoint, "/api/token"),
         headers: {
             "Authorization": AUTH_HEADER,
             "Content-Type": "application/x-www-form-urlencoded"
@@ -82,14 +92,18 @@ app.post("/refresh", function (req, res) {
         form: form_data
     }, function (err, response, body) {
         if (err) {
+            debug("refresh: error: %s", err);
             res.status(500).send("Internal Server Error");
             return;
         }
 
         if (response.statusCode != 200) {
+            debug("refresh: response: %s", response.statusCode);
             res.status(550).send("Permission Denied");
             return;
         }
+
+        debug("refresh: body: %s", body);
 
         res.status(response.statusCode).set({
             "Content-Type": "application/json"
@@ -97,6 +111,36 @@ app.post("/refresh", function (req, res) {
     });
 });
 
+app.post("/api/token", function (req, res) {
+    debug("token request:", req.body);
+
+    res.status(200).set({
+        "Content-Type": "application/json"
+    });
+
+    switch (req.body.grant_type) {
+        case "authorization_code": {
+            res.status(200).set({
+                "Content-Type": "application/json"
+            }).send({
+                "refresh_token": "REFRESH TOKEN"
+            });
+        } break;
+        case "refresh_token": {
+            res.status(200).set({
+                "Content-Type": "application/json"
+            }).send({
+                "access_token": "ACCESS TOKEN"
+            });
+        } break;
+        default: {
+            res.status(550).set({
+                "Content-Type": "text/html"
+            }).send("<html><body><h1>Access Denied</h1></body></html>");
+        } break;
+    }
+});
+
 var server = app.listen(1234, function () {
-    console.log("Token swap service listening on http://%s:%d", server.address().address, server.address().port);
+    debug("Token swap service listening on http://%s:%d", server.address().address, server.address().port);
 });
